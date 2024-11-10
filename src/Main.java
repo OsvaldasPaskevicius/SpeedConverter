895729dffe1de9a5790121c57f8cccfffa98aaf7
MrOsvaldas <emigrantas0202@icloud.com>
2024-11-10 11:27:00 +0200

    Su antra dalimi

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,22 @@ public class Main {
         double milesPerHour = kilometresPerHour / 1.60934;
         return Math.round(milesPerHour);
     }
-    public static void main(String[] args) {
-        System.out.println(toMilesPerHour(1.5));
 
+    public static void printConversion(double kilometresPerHour) {
+        if (kilometresPerHour < 0) {
+            System.out.println("Invalid value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometresPerHour);
+            System.out.println(kilometresPerHour + "km/h = " + milesPerHour + "mi/h");
+        }
+    }
+
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
     }
 }
+
